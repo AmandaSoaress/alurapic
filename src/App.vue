86e4a299337348067716d0b1cd88d8e6e303966d
Amanda Santos <113608404+AmandaSoaress@users.noreply.github.com>
2024-01-30 12:44:31 -0300

    rotas

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="corpo">
-    
+   
+    <meu-menu :rotas="routes"/>
+
+    <Transition name="pagina">
+    <router-view></router-view>
+  </Transition>
   </div>
 </template>
 
-<script>
-export default{
+<script> 
+import {routes} from'./routes';
+import Menu from './components/shared/menu/Menu.vue';
 
-}
+export default {
+  components:{
+'meu-menu': Menu
+  },
+  data(){
+    return{
+      routes
+    }
+  }
+};
 </script>
 
 <style>
@@ -17,4 +32,10 @@ export default{
   margin: 0 auto;
 }
 
+.pagina-enter,.pagina-leave-active{
+  opacity: 0;
+}
+.pagina-enter-active, .pagina-leave-active{
+  transition: opacity .4s;
+}
 </style>
